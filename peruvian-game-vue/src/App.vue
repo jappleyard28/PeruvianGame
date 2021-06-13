f497ef7089a1403be4e4b6d26b90934d67acaa48
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2021-06-13 17:14:27 +0100

    added language and sidebar components

--- peruvian-game-vue/src/App.vue
+++ peruvian-game-vue/src/App.vue
@@ -2,16 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <router-view />
+    <Language />
+    <SideBar />
   </div>
 </template>
 
 <script>
+import HelloWorld from './pages/HelloWorld.vue'
+import Language from './components/Language.vue'
+import SideBar from './components/SideBar.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Language,
+    SideBar
+  }
 }
 </script>
 
---
+++ peruvian-game-vue/src/components/SideBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="sidenav">
+      <!-- change hrefs to take the user to each page -->
+      <a href="Home page" id="navLogo">(Logo)</a>
+      <a href="home page">Home</a>
+      <a href="https://www.google.co.uk">Info</a>
+      <a href="quiz page">Quiz</a>
+      <a href="game page">Game</a>
+      <!--<a href="#">Difficulty</a> remove as difficulty should be separate for the quiz and game-->
+      <a href="leaderboard page">Leaderboard</a>
+      <a href="about us page">About us</a>
+      <a href="contact us page">Contact us</a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  methods:{
+    sideBar()
+    {
+      alert("test");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body {
+    font-family: "Lato", sans-serif;
+  }
+
+  .sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: white;
+    color: orange;
+    overflow-x: hidden;
+    padding-top: 20px;
+  }
+
+  .sidenav a {
+    padding: 0px 0px 15px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+  }
+
+  .sidenav a:hover {
+    color: orange;
+  }
+
+  .main {
+    margin-left: 200px; /* Same as the width of the sidenav */
+  }
+
+  #navLogo {
+    padding: 50px;
+    color: blue;
+  }
+</style>
