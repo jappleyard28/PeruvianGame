fb3b07596fec45f770dc0e46059cd5caae87075a
Nathan <cpz18hbu@uea.ac.uk>
2021-06-16 00:35:03 +0100

    updated server code to have most endpoints +readme

--- peruvian-game-server/README.md
+++ peruvian-game-server/README.md
@@ -10,23 +10,295 @@ To start the server run
 yarn start
 ```
 
+## Filling out the Questions
+
+To fill out the questions you can look at the ones that are already in the `questions.json` file to see the structure.
+To fill the id field you can make a get request on `/api/uuid` with `amount` as a parameter to specify how many ids you want it to print.
+
 ## API
 
-### `/api/leaderboard`
+### GET `/api/leaderboard`
 
 #### Parameters
 
-* `limit`: (DEFAULT: 15) to specify the limit on the number of results returned
-* `page`: (DEFAULT: 1) to specify an offset if a lmiit is used
+* queries
+  * `limit`: (DEFAULT: 15) to specify the limit on the number of results returned
+  * `page`: (DEFAULT: 1) to specify an offset if a lmiit is used
 
 #### Sample Response
 
 ```json
 {
-  leaderboard: [
+  "leaderboard": [
     {"name": "john smith", "score": 1000},
     {"name": "peter johnson", "score": 800}
   ],
-  page: 1
+  "page": 1
+}
+```
+
+### GET `/api/quiz`
+
+#### Parameters
+
+* queries
+  * `type`: to specify the quiz type either nutrition or math
+  * `difficulty`: to specify the difficulty of the quiz
+
+#### Sample Response
+
+##### type=math
+
+GET `/api/quiz?type=math&difficulty=1`
+
+```json
+{
+    "questions": [
+        "4 × 4",
+        "9 × 3",
+        "1 + 5",
+        "6 × 5",
+        "2 × 1",
+        "7 − 5",
+        "1 × 8",
+        "2 − 9",
+        "8 + 8",
+        "6 + 8"
+    ],
+    "session_token": "36387b49-6437-455e-9237-4fb41c358712"
+}
+```
+
+GET `/api/quiz?type=math&difficulty=2`
+
+```json
+{
+    "questions": [
+        "( 2 + 0 ) − 7",
+        "( 9 × 7 ) − 6",
+        "( 1 × 5 ) − 9",
+        "( 5 − 3 ) + 1",
+        "( 5 + 5 ) − 1",
+        "( 2 + 7 ) − 3",
+        "( 3 − 2 ) − 8",
+        "( 4 + 9 ) − 3",
+        "( 3 − 1 ) − 1",
+        "( 5 × 8 ) − 4"
+    ],
+    "session_token": "0d94be1f-e7a8-44ec-a50c-ac9e96b92a46"
+}
+```
+
+GET `/api/quiz?type=math&difficulty=3`
+
+```json
+{
+    "questions": [
+        "( ( 2 + 6 ) × 7 ) × 5",
+        "( ( 5 × 4 ) − 6 ) × 0",
+        "( ( 2 + 0 ) − 8 ) − 6",
+        "( ( 4 + 6 ) × 4 ) × 1",
+        "( ( 1 + 7 ) − 0 ) − 3",
+        "( ( 2 − 2 ) − 9 ) + 0",
+        "( ( 7 − 9 ) × 9 ) + 8",
+        "( ( 9 − 8 ) − 3 ) + 6",
+        "( ( 5 − 9 ) − 2 ) × 4",
+        "( ( 6 × 6 ) × 4 ) + 3"
+    ],
+    "session_token": "a29f5137-cdd5-4494-a92d-ba5f3c9731aa"
+}
+```
+
+##### type=nutrition
+
+GET `/api/quiz?type=nutrition&difficulty=1`
+
+```json
+{
+    "questions": [
+        {
+            "id": "6ee2b7e0-f283-42b8-a94f-96e79c5aab0b",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "149fb705-34a7-454d-8f73-573a04a0387d",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "5c63f5cf-247a-4b8d-b0a4-e7235b376568",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "fdd06a20-3fc2-49e4-b21b-aa5b36066414",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "5393b83f-1d78-4e42-961c-b419872d2814",
+            "question": "What is an apple?",
+            "type": "multiple-choice",
+            "difficulty": 1,
+            "options": [
+                "A fruit",
+                "an orange"
+            ]
+        },
+        {
+            "id": "6a25ca2a-3d63-471d-89ba-49e0556ea95c",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "10862e1d-442c-418a-903d-d8435cb2f6ac",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "ff3b6a02-e3f9-4d33-b6cf-62a788b3d84c",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "6106636b-bf54-4d3c-8678-0181f032eff1",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        },
+        {
+            "id": "454d5245-f621-42b6-803a-954938d7859e",
+            "question": "Is a mango a fruit or vegetable?",
+            "type": "text",
+            "difficulty": 1
+        }
+    ]
+}
+```
+
+### POST `/api/answer`
+
+##### Parameters
+
+#### Maths
+
+* body
+  * `answers`: array of the user's answers. Array of numbers for maths
+  * `type`: the quiz type
+  * `session_token`: the session token which is recieved from the question
+  * `name`: (DEFAULT: `'Anonymous'`) the user's name so it can be put on leaderboard
+
+##### Sample Request
+
+REF 1
+
+```json
+{
+  "answers": [10, 1, 4, 6, 8, 9, 8, 22, 10, 11],
+  "type": "math",
+  "session_token": "3b9eae62-e4a7-43c1-83c5-ac75a341e0ca",
+  "name": "Tesst name"
+}
+```
+
+##### Sample Response
+
+From request in REF 1
+
+```json
+{
+    "results": {
+        "score": 1,
+        "results": [
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        "answers": [
+            10,
+            4,
+            7,
+            16,
+            12,
+            -1,
+            0,
+            -6,
+            1,
+            -5
+        ]
+    }
+}
+```
+
+##### Nutrition
+
+* body
+  * `answers`: array of the user's answers. Array objects that have two string values, `question` and `value` if sending answers for the nutrition quiz.
+  * `type`: the quiz type
+  * `name`: (DEFAULT: `'Anonymous'`) the user's name so it can be put on leaderboard
+
+##### Sample Request
+
+REF 3
+
+```json
+{
+  "name": "",
+  "answers": [
+    { "question": "5393b83f-1d78-4e42-961c-b419872d2814", "value": "A fruit" },
+    { "question": "10862e1d-442c-418a-903d-d8435cb2f6ac", "value": "Potatoes" }
+  ]
+}
+
+```
+
+##### Sample Response
+
+From request in REF 1
+
+```json
+{
+    "results": {
+        "score": 1,
+        "results": [
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        "answers": [
+            "A fruit",
+            "Walrus",
+            "Bird",
+            "Meat",
+            "Beans",
+            "50",
+            "1 cup",
+            "3 hours",
+            "Steak",
+            "Potatoes"
+        ]
+    }
 }
 ```
--- peruvian-game-server/server.js
+++ peruvian-game-server/server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const fs = require('fs');
+const { body, query, validationResult, oneOf } = require('express-validator');
 const history = require('connect-history-api-fallback');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PERUVIAN_PORT;
+const PORT = process.env.PERUVIAN_PORT || 3000;
 const PATH_TO_VUE = '../peruvian-game-vue';
+
+const NUMBER_OF_QUESTIONS = 10;
+const SESSION_TOKEN_LIFESPAN = 1000 * 60 * 60 * 6;
+const DIFFICULTY_TYPES = Object.freeze({ EASY: 1, MEDIUM: 2, HARD: 3 });
+const QUIZ_TYPES = Object.freeze({ MATH: 'math', NUTRITION: 'nutrition' });
 
 const SESSION_QUESTIONS = {
 	// format
 	/*
-  "session_uuid": {
-    expires: "timestap",
-    difficulty: 1
-    answers: [
-      55,
-      67
-    ]
-  }
-  */
+	"session_uuid": {
+		expires: "timestap",
+		difficulty: 1
+		answers: [
+			55,
+			67
+		]
+	}
+	*/
 };
 
 const getDb = (filePath, encoding = 'utf8') =>
@@ -30,18 +37,18 @@ const getDb = (filePath, encoding = 'utf8') =>
 		});
 	}).then(JSON.parse);
 
-const updateDb = (content) => fs.writeFileSync(filePath, JSON.stringify(content));
+const updateDb = (filePath, content) => fs.writeFileSync(filePath, JSON.stringify(content, null, 4));
 
 const dumpExpiredQuestions = () => {
-  console.log(Date.now().toString()+": Starting dump of expired sessions.");
-  let count = 0;
+	console.log(Date.now().toString() + ': Starting dump of expired sessions.');
+	let count = 0;
 	for (const questionSet in SESSION_QUESTIONS) {
 		if (SESSION_QUESTIONS[questionSet].expires <= Date.now()) {
 			delete SESSION_QUESTIONS[questionSet];
-      count++;
+			count++;
 		}
 	}
-  console.log(`${Date.now().toString()}: Dump finshed. Sessions removed: ${count}`);
+	console.log(`${Date.now().toString()}: Dump finshed. Sessions removed: ${count}`);
 };
 
 // Middleware to serve the vue site
@@ -50,78 +57,326 @@ const staticFileMiddleware = express.static(`${PATH_TO_VUE}/dist`);
 // call for unredirected requests
 app.use(staticFileMiddleware);
 
+app.use(express.json());
+
+const getObjValues = (obj) => Object.keys(obj).map((key) => obj[key]);
+
 // Any API calls
 
 // return list of leaderboard at liimit specified
-app.get('/api/leaderboard', async (req, res) => {
-	const db = await getDb('leaderboard.json');
-	const sortedLeaderboard = db.leaderboard.sort((a, b) => a.score - b.score);
-	const limit = req.query.limit || 15;
-	const page = (req.query.page > 0 ? req.query.limit : 1) || 1;
+app.get(
+	'/api/leaderboard',
+	[ query('limit', 'limit must be an integer').isInt(), query('page', 'page must be an integer').isInt() ],
+	async (req, res) => {
+		//validation
+		const validResult = validationResult(req);
+		if (!validResult.isEmpty()) return res.json(validResult.mapped());
 
-	const leaderboardArray = sortedLeaderboard.slice(limit * (page - 1), limit);
-	return res.json({ leaderboard: leaderboardArray, page: page });
-});
+		const db = await getDb('leaderboard.json');
+		const sortedLeaderboard = db.sort((a, b) => a.score - b.score);
+		const limit = req.query.limit || 15;
+		const page = (req.query.page > 0 ? req.query.limit : 1) || 1;
 
-	// return quiz data
-app.get('/api/quiz', async (req, res) => {
-  //validate type
-	const quizType = req.query.type;
-	if (typeof quizType == 'undefined' || !(quizType in [ 'math', 'nutrition' ])) {
-		return res.status(400).json({ message: 'invlaid type for quiz' });
+		const leaderboardArray = sortedLeaderboard.slice(limit * (page - 1), limit);
+		return res.json({ leaderboard: leaderboardArray, page: page });
+	}
+);
+
+const getRandInt = (max) => Math.floor(Math.random() * max);
+
+const createAdditionSet = (a = null, b = null) => {
+	a = a || getRandInt(10);
+	b = b || getRandInt(10);
+	return {
+		num1: a,
+		num2: b,
+		operator: '+',
+		answer: a + b
+	};
+};
+
+const createSubtractionSet = (a = null, b = null) => {
+	a = a || getRandInt(10);
+	b = b || getRandInt(10);
+	return {
+		num1: a,
+		num2: b,
+		operator: '−',
+		answer: a - b
+	};
+};
+const createMultiplicationSet = (a = null, b = null) => {
+	a = a || getRandInt(10);
+	b = b || getRandInt(10);
+	return {
+		num1: a,
+		num2: b,
+		operator: '×',
+		answer: a * b
+	};
+};
+
+const generateMathQuiz = (difficulty) => {
+	const SESSION_TOKEN = uuidv4();
+	const questions = [];
+	const answers = [];
+
+	const qGenTypes = [ createAdditionSet, createSubtractionSet, createMultiplicationSet ];
+
+	// fill question and answer arrays
+	for (let i = 0; i < NUMBER_OF_QUESTIONS; i++) {
+		const questionParts = [];
+		let prevPartAns;
+		// create each question part (number depends on how difficult)
+		for (let difficultyLevel = 1; difficultyLevel <= difficulty; difficultyLevel++) {
+			// choose random question type and generate set
+			let part;
+			// use 2 new values if
+			if (questionParts.length == 0) {
+				part = qGenTypes[getRandInt(qGenTypes.length)]();
+				console.log(''); //REMOVEME
+			} else {
+				part = qGenTypes[getRandInt(qGenTypes.length)](prevPartAns);
+			}
+			questionParts.push(part);
+			prevPartAns = part.answer;
+			console.log(part); //REMOVEME
+		}
+		answers.push(questionParts[questionParts.length - 1].answer);
+
+		let question = '';
+		// combine questino parts to form question
+		questionParts.forEach((part) => {
+			if (question.length === 0) {
+				question = `${part.num1} ${part.operator} ${part.num2}`;
+			} else {
+				question = `( ${question} ) ${part.operator} ${part.num2}`;
+			}
+		});
+		questions.push(question);
 	}
 
-  // get questionset for that type
-  // if maths do sessions stuff
-  // return res 
-	return res.json({message: 'nothing yet...'});
-});
+	console.log(answers); //REMOVEME
 
-app.post('/api/answer', (req, res) => {
-	// take quiz answers
-});
+	// create and store server side information for generated question
+	const session = {
+		expires: Date.now() + SESSION_TOKEN_LIFESPAN,
+		difficulty: difficulty,
+		answers: answers
+	};
+	SESSION_QUESTIONS[SESSION_TOKEN] = session;
+	return [ SESSION_TOKEN, questions ];
+};
 
-// endpoint for recieving user answers to generated maths questions
-app.post('/api/mathscore', async (req, res) => {
-	const SESSION_TOKEN = req.body.session_token;
-	const userAnswers = req.body.answers;
-	const name = req.body.name || 'Anonymous';
+const generateNutritionQuiz = async (difficulty) => {
+	const dbQuestions = (await getDb('questions.json'))[QUIZ_TYPES.NUTRITION];
+	const questions = [];
+	const checkedIndexes = [];
+	let is = [];
+	while (questions.length < NUMBER_OF_QUESTIONS) {
+		const index = getRandInt(dbQuestions.length);
 
-	// validate submission
-	if (typeof SESSION_TOKEN == 'undefined' || !(SESSION_TOKEN in SESSION_QUESTIONS)) {
-		return res.status(410).json({ message: 'invalid session token' });
+		// grab a different question if already have the question
+		if (checkedIndexes.includes(index) || dbQuestions[index].difficulty != difficulty) continue;
+
+		const dbQuestion = dbQuestions[index];
+		checkedIndexes.push(index);
+
+		// grab a different question if difficulty not what is needed
+		if (dbQuestions[index].difficulty != difficulty) continue;
+
+		const question = {};
+		question.id = dbQuestion.id;
+		question.question = dbQuestion.question;
+		question.type = dbQuestion.type;
+		question.difficulty = difficulty;
+
+		//add type specific values
+		if (dbQuestion.type === 'multiple-choice') question.options = dbQuestion.options;
+
+		questions.push(question);
 	}
+	return questions;
+};
 
-	const session = SESSION_QUESTIONS.SESSION_TOKEN;
-	const answers = sesssion.answers;
-	if (userAnswers.length != answers.length || !userAnswers.every((ans) => typeof ans == 'number')) {
-		return res.status(400).json({ message: 'answers length needs to match number of questions and must all be numbers' });
+// return quiz data
+app.get(
+	'/api/quiz',
+	[
+		query('type', 'must have a quiz type').not().isEmpty(),
+		query('type', `type must be of ${getObjValues(QUIZ_TYPES)}`).custom((value) =>
+			getObjValues(QUIZ_TYPES).includes(value)
+		),
+		query('difficulty', 'must have a difficulty for the quiz').not().isEmpty(),
+		query('difficulty', 'difficulty must be an integer').isInt()
+	],
+	async (req, res) => {
+		//validate type
+		const validResult = validationResult(req);
+		if (!validResult.isEmpty()) return res.status(400).json(validResult.mapped());
+
+		const quizType = req.query.type;
+		const difficulty = parseInt(req.query.difficulty) || null;
+
+		// validate quiz type provided and existsin db
+		if (typeof quizType === 'undefined') return res.status(400).json({ message: 'missing quiz type' });
+		if (!getObjValues(QUIZ_TYPES).includes(quizType))
+			return res.status(400).json({ message: 'invlaid type for quiz' });
+
+		// validate quiz difficulty level provided and exists on server
+		if (typeof difficulty === 'undefined') return res.status(400).json({ message: 'missing quiz difficulty' });
+		// check and convert difficulty to int
+		if (difficulty == null || !getObjValues(DIFFICULTY_TYPES).includes(difficulty))
+			return res.status(400).json({ message: 'invalid difficulty for quiz' });
+
+		// get questionset for that type
+		const quiz = {};
+		switch (quizType) {
+			case QUIZ_TYPES.MATH:
+				const [ SESSION_TOKEN, questions ] = generateMathQuiz(difficulty);
+				quiz.questions = questions;
+				quiz.session_token = SESSION_TOKEN;
+				break;
+			case QUIZ_TYPES.NUTRITION:
+				quiz.questions = await generateNutritionQuiz(difficulty);
+				break;
+		}
+		// return res
+		return res.json(quiz);
 	}
+);
+
+// to make results from user answers for genereated amths questions
+const checkMathQuiz = (SESSION_TOKEN, userAnswers) => {
+	const session = SESSION_QUESTIONS[SESSION_TOKEN];
+	const answers = session.answers;
 
 	// check answers
-	const results = { score: 0, results: [] };
+	const results = { score: 0, results: [], answers: answers };
 	for (let i = 0; i < answers.length; i++) {
-		if (answers[i] == userAnswers[i]) {
+		if (answers[i] === userAnswers[i]) {
 			results.score += session.difficulty; // CHANGE TO WHATEVER SCORING SYSTEM IS WANTED
 			results.results.push(true);
 		} else {
 			results.results.push(false);
 		}
 	}
-	// find score
-
-	// store score
-	const db = await getDb('leaderboard.json');
-	const leaderboardEntry = { name: name, score: results.score, timestamp: Date.now() };
-	db.leaderboard.push(leaderboardEntry);
-	updateDb(db);
 
 	// remove session question
-	delete SESSION_QUESTIONS.SESSION_TOKEN;
+	delete SESSION_QUESTIONS[SESSION_TOKEN];
 
 	// respond with score and results
-	results.answers = userAnswers;
-	return res.json(results);
+	return results;
+};
+
+const checkNutritionQuiz = async (userAnswers) => {
+	const results = { score: 0, results: [], answers: [] };
+	const dbAnswers = await getDb('questions.json')[QUIZ_TYPES.NUTRITION];
+
+	userAnswers.forEach((userAnswer) => {
+		// validate if question id valid
+		const question = dbAnswers.find((dbQ) => dbQ.id === userAnswer.question) || null;
+		if (question !== null && question.answer === userAnswer) {
+			results.score += question.difficulty;
+			results.answers.push(question.answer);
+			results.results.push(true);
+		} else {
+			answers.push('INVALID QUESTION');
+			results.results.push(false);
+		}
+	});
+
+	return results;
+};
+
+app.post(
+	'/api/answer',
+	[
+		body('name').escape().trim(),
+		oneOf(
+			[
+				[
+					body('type').equals(QUIZ_TYPES.NUTRITION),
+					body('answers.*.question', 'question must be set to a valid UUID')
+						.isUUID()
+						.custom((value, { req }) => {}),
+					body('answers.*.value', `value must be a string for ${QUIZ_TYPES.NUTRITION} types`).isString()
+				],
+				[
+					body('type').equals(QUIZ_TYPES.MATH),
+					body(
+						'answers.*',
+						`answers must be in the form of integers for the ${QUIZ_TYPES.MATH} type quiz`
+					).isInt(),
+					body('session_token', 'session token must be a valid UUID').isUUID()
+				]
+			],
+			'Please match the parameters to the quiz type'
+		)
+	],
+	async (req, res) => {
+		const validResult = validationResult(req);
+		const hasErrors = !validResult.isEmpty();
+		if (hasErrors)
+			return res.status(400).json(validResult.mapped());
+
+		const name = req.body.name || 'Anonymous';
+		const answers = req.body.answers;
+		const type = req.body.type;
+
+		// validate quiz type
+		if (!getObjValues(QUIZ_TYPES).includes(type)) return res.status(400).json({ message: 'quiz type not valid' });
+
+		let results;
+		switch (type) {
+			case QUIZ_TYPES.MATH:
+				// validate session token
+				const SESSION_TOKEN = req.body.session_token;
+				if (typeof SESSION_TOKEN === 'undefined' || !(SESSION_TOKEN in SESSION_QUESTIONS))
+					return res.status(410).json({ message: 'invalid session token' });
+
+				// get results
+				results = checkMathQuiz(SESSION_TOKEN, answers);
+				break;
+
+			case QUIZ_TYPES.NUTRITION:
+				// validate type of answer list to correct answer list
+				if (!answers.every((ans) => typeof ans.question === 'string' && typeof ans.value === 'string'))
+					return res.status(400).json({ message: 'answers do not match the expected type' });
+
+				// validate length and type of answer list to correct answer list
+				if (!answers.every((ans) => typeof ans === 'number'))
+					return res
+						.status(400)
+						.json({ message: 'answers length needs to match number of questions and must all be numbers' });
+
+				//get results
+				results = await checkNutritionQuiz(answers);
+				break;
+
+			default:
+				return res.status(400).json({ message: 'invalid quiz type' });
+		}
+
+		// store score
+		const leaderboard = await getDb('leaderboard.json');
+		const leaderboardEntry = { name: name, score: results.score, timestamp: Date.now() };
+		leaderboard.push(leaderboardEntry);
+		updateDb('leaderboard.json', leaderboard);
+
+		return res.json(results);
+	}
+);
+
+// REMOVEME
+app.get('/api/uuid', [ query('amount').not().isEmpty().isInt() ], (req, res) => {
+	const result = validationResult(req);
+	if (!result.isEmpty()) return res.status(400).json({ message: 'bad amount', report: validationResult.mapped() });
+	const amount = parseInt(req.query.amount);
+
+	const ids = [];
+	for (i = 0; i < amount; i++) ids.push(uuidv4());
+	return res.json(ids);
 });
 
 // Allow for use of history api
@@ -135,8 +390,8 @@ app.use(
 app.use(staticFileMiddleware);
 
 // Make server listen on port specified by environment variable or default port 3000
-app.listen(PORT || 3000, () => {
-	console.log('Server started');
+app.listen(PORT, () => {
+	console.log(`Server started on port http://localhost:${PORT}`);
 	// repeat calls every 2hrs
 	setInterval(() => {
 		dumpExpiredQuestions();
