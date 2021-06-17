aecf59fb19d3e36d653f9daf88af1dd4b00cd0b9
Nathan <cpz18hbu@uea.ac.uk>
2021-06-17 12:01:52 +0100

    update home page

--- peruvian-game-vue/src/pages/Home.vue
+++ peruvian-game-vue/src/pages/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    HOME
-    <router-link to="/">HOME</router-link>
-    <router-link to="/leaderboard">LEADERBOARD</router-link>
+    <h1>Welcome To Peruvian Game</h1>
+
+    <h2>Our Goal</h2>
+
+    <p>We want let people in Lobitos and Piedritas have fun while learning about nutrition and testing their maths skills.</p>
+
+    <h2>How to play</h2>
+
+    <p> Click on Quiz in the side bar, select a quiz to do and start thinking! </p>
+
+    <h2>Our Top Scorers</h2>
+
+    <!-- <leaderboard-component /> -->
+
   </div>
 </template>
 
 <script>
+import LeaderboardComponent from './Leaderboard.vue';
 export default {
-  name: "Hello",
-  components: {}
+  name: "Home",
+  components: {}//LeaderboardComponent}
 };
 </script>
 
