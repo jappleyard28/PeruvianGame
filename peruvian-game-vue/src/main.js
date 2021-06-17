f10eeae125454f981243646137e01176d06019fe
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2021-06-17 06:22:09 +0100

    added vue-i18n

--- peruvian-game-vue/src/main.js
+++ peruvian-game-vue/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import VueI18n from 'vue-i18n'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 Vue.use(VueI18n)
 
 new Vue({
   router,
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
