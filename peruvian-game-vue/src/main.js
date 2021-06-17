ddb277889d283c51235cf66b3b657d65107ae978
jappleyard28 <44617590+jappleyard28@users.noreply.github.com>
2021-06-17 05:50:00 +0100

    added stuff

--- peruvian-game-vue/src/main.js
+++ peruvian-game-vue/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
+Vue.use(VueI18n)
 
 new Vue({
   router,
